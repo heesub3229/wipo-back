97c68bb903f281108abcbb46d537b57f55aa3f62
heesub3229 <heesub3229@naver.com>
2024-11-11 15:01:24 +0900

    first commit

---
+++ src/main/java/com/wipo/WipoApplication.java
@@ -0,0 +1,13 @@
+package com.wipo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WipoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WipoApplication.class, args);
+	}
+
+}
